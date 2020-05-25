f2785e195a0305de1349e32bac26af1497092df2
soniasriram <65900219+soniasriram@users.noreply.github.com>
2020-05-25 17:02:18 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,10 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
 
+from time import time
+from email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +23,26 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+def ds_svm(features_train, features_test, labels_train, labels_test, kernel='linear', C=1.0):
+    # the classifier
+    clf = SVC(kernel=kernel, C=C)
 
+    # train
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "\ntraining time:", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
+    # predict
+    t0 = time()
+    pred = clf.predict(features_test)
+    print "predicting time:", round(time()-t0, 3), "s"
 
-#########################################################
+    accuracy = accuracy_score(pred, labels_test)
+
+    print '\naccuracy = {0}'.format(accuracy)
+    return pred
+
+pred = ds_svm(features_train, features_test, labels_train, labels_test)
+
 
 
